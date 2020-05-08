3d133965c79bad0815a714680674d470192ffa30
Rustem Islamuratov <rustem.islamuratov@gmail.com>
2020-05-08 11:39:17 +0500

    Update q069

--- CodingInterviewQuestions/q069.js
+++ CodingInterviewQuestions/q069.js
@@ -3,6 +3,7 @@
 // Given an array and a number k that's smaller than the length of the array, rotate the array to the right k elements
 // in-place.
 
+// APPROACH 1
 const rotate = (arr, k) => {
   for (let i = 0; i < k; i++) {
     for (let i = arr.length - 1; i > 0; i--) {
@@ -15,4 +16,26 @@ const rotate = (arr, k) => {
   return arr;
 }
 
+// APPROACH 2
+const rotate2 = (arr, k) => {
+  const reverse = (arr, i, j) => {
+    while (i < j) {
+      arr[i] += arr[j];
+      arr[j] = -(arr[j] - arr[i]);
+      arr[i] -= arr[j];
+      i++;
+      j--;
+    }
+
+    return arr;
+  }
+
+  reverse(arr, 0, arr.length - k - 1);
+  reverse(arr, arr.length - k, arr.length - 1);
+  reverse(arr, 0, arr.length - 1);
+
+  return arr;
+}
+
 console.log([8, 9, 1, 2, 3, 4, 5, 6, 7].join(','), rotate([1, 2, 3, 4, 5, 6, 7, 8, 9], 2).join(','));
+console.log([8, 9, 1, 2, 3, 4, 5, 6, 7].join(','), rotate2([1, 2, 3, 4, 5, 6, 7, 8, 9], 2).join(','));
